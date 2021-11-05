705fe717a6c74f11c7836a57a322c4769657de97
ambrojessica <ambrojessica0@gmail.com>
2021-11-05 14:00:03 -0500

    working but need to fix button

--- src/mocks/List.js
+++ src/mocks/List.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const List = (props) => {
+  const open = () => {
+    
+  }
   return (
     <div>
       <h3>{props.character.name}</h3>
+      <button onClick={open}>{props.character.birth_year}</button>
+      <ul>
+        <li>{props.character.gender}</li>
+        <li>{props.character.height}</li>
+        <li>{props.character.mass}</li>
+        <li>{props.character.skin_color}</li>
+      </ul>
+      
     </div>
   )
 }
